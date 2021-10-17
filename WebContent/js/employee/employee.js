7561c95b2efbe9bbfbd3577d5bc7a4aeb7398a06
hjeom <nara1030@naver.com>
2021-10-17 21:31:29 +0900

    Revert "first commit"
    This reverts commit b0468628a2003aec90d7b95050ca4084dfb30449.

--- WebContent/js/employee/employee.js
+++
@@ -1,7 +0,0 @@
-// window.onload vs. document.onload
-window.onload = function() {
-	document.getElementById("btn").addEventListener('click', function() {
-		var name = document.getElementById("emp_name").value;
-		console.log(name);
-	});
-};
