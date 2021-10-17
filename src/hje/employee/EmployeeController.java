b0468628a2003aec90d7b95050ca4084dfb30449
hjeom <nara1030@naver.com>
2021-10-17 21:17:19 +0900

    first commit

---
+++ src/hje/employee/EmployeeController.java
@@ -0,0 +1,38 @@
+package hje.employee;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import hje.employee.service.Employees;
+
+public class EmployeeController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// super.doGet(req, resp);
+		
+		System.out.println("***** 직원 관리 *****");		
+		doGetProcess(req, resp);
+	}
+	
+	/**
+	 * doGet 메소드의 비즈니스 로직을 처리한다.
+	 * 
+	 * @param req
+	 * @param resp
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void doGetProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Employees employees = new Employees();
+		
+		req.setAttribute("employees", employees.getEmployees());
+		
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/employee/employee.jsp");
+		requestDispatcher.forward(req, resp);
+	}
+}
